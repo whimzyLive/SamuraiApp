9168b23d19691db57ac475a315237a785776a534
Rushi Patel <rp970.patel@gmail.com>
2021-10-09 19:23:17 +1030

    Add initial Samurais

---
+++ SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; } = new();
+    }
+}
---
+++ SamuraiApp.UI/Program.cs
@@ -0,0 +1,39 @@
+﻿using SamuraiApp.Domain;
+using SamuraiApp.Data;
+using System.Linq;
+using System;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new();
+        static void Main(string[] args)
+        {
+            _context.Database.EnsureCreated();
+
+            Console.WriteLine("Enter a Samurai Name To Save to DB:");
+            string samurai = Console.ReadLine();
+            AddSamurai(samurai);
+
+            Console.WriteLine("All Samuraies: ");
+            GetSamurais();
+
+
+        }
+
+        private static void AddSamurai(string SamuraiName)
+        {
+            var samurai = new Samurai { Name = SamuraiName };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void GetSamurais()
+        {
+            var samurais = _context.Samurais.ToList();
+
+            samurais.ForEach(sam => Console.WriteLine(sam.Name));
+        }
+    }
+}
